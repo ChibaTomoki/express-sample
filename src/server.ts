909670e88fbd42fa194c07220b072ee05b82e8d1
ChibaTomoki <michiamo.t1@gmail.com>
2025-03-16 00:27:49 +0900

    add: cors設定

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 import express, { Router } from "express";
 import getTodoRouter from "./todo/todo.route";
 
 const app = express();
 const prisma = new PrismaClient();
 const apiV1Router = Router();
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+};
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Express Server is running");
