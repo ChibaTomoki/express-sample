c0285b0636dd9a69250a4b24a909731a6c96927f
ChibaTomoki <michiamo.t1@gmail.com>
2025-03-15 20:44:29 +0900

    refactor: /api/v1を共通化

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { Router } from "express";
 import getTodoRouter from "./todo/todo.route";
 
 const app = express();
 const prisma = new PrismaClient();
-
+const apiV1Router = Router();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Express Server is running");
 });
 
-app.use("/api/v1/todos", getTodoRouter(prisma));
+apiV1Router.use("/todos", getTodoRouter(prisma));
+app.use("/api/v1", apiV1Router);
 
 app.listen(process.env.PORT || 3000, () => {
   if (!process.env.PORT)
